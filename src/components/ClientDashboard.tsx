abbbef677a1f156cb19f4fa618c4addafb09a542
iLi-Media-Group <theilimediagroup@gmail.com>
2025-05-29 09:37:33 -0300

    Add export to ClientDashboard component

--- src/components/ClientDashboard.tsx
+++ src/components/ClientDashboard.tsx
@@ -1,1 +1,10 @@
-// Full updated file content shown above
+import React from 'react';
+
+export function ClientDashboard() {
+  return (
+    <div>
+      <h1>Client Dashboard</h1>
+      {/* Add dashboard content here */}
+    </div>
+  );
+}
