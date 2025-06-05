c41215c5b34e211848613bdec30200bdf672ce1d
iLi-Media-Group <theilimediagroup@gmail.com>
2025-06-05 12:18:34 -0300

    Fix syntax error in ProducerDashboard.tsx

--- src/components/ProducerDashboard.tsx
+++ src/components/ProducerDashboard.tsx
@@ -1,74 +1,66 @@
-Here's the fixed version with the missing closing bracket:
-
-```javascript
 {selectedTrack && showDeleteDialog && (
-        <DeleteTrackDialog
-          isOpen={showDeleteDialog}
-          onClose={() => {
-            setShowDeleteDialog(false);
-            setSelectedTrack(null);
-          }}
-          trackTitle={selectedTrack.title}
-          onConfirm={confirmDeleteTrack}
-        />
-      )}
+  <DeleteTrackDialog
+    isOpen={showDeleteDialog}
+    onClose={() => {
+      setShowDeleteDialog(false);
+      setSelectedTrack(null);
+    }}
+    trackTitle={selectedTrack.title}
+    onConfirm={confirmDeleteTrack}
+  />
+)}
 
-      {selectedTrack && showTrackProposalsDialog && (
-        <TrackProposalsDialog
-          isOpen={showTrackProposalsDialog}
-          onClose={() => {
-            setShowTrackProposalsDialog(false);
-            setSelectedTrack(null);
-          }}
-          track={selectedTrack}
-        />
-      )}
+{selectedTrack && showTrackProposalsDialog && (
+  <TrackProposalsDialog
+    isOpen={showTrackProposalsDialog}
+    onClose={() => {
+      setShowTrackProposalsDialog(false);
+      setSelectedTrack(null);
+    }}
+    track={selectedTrack}
+  />
+)}
 
-      {showRevenueBreakdown && (
-        <RevenueBreakdownDialog
-          isOpen={showRevenueBreakdown}
-          onClose={() => setShowRevenueBreakdown(false)}
-          userId={user?.id}
-        />
-      )}
+{showRevenueBreakdown && (
+  <RevenueBreakdownDialog
+    isOpen={showRevenueBreakdown}
+    onClose={() => setShowRevenueBreakdown(false)}
+    userId={user?.id}
+  />
+)}
 
-      {selectedProposal && showNegotiationDialog && (
-        <ProposalNegotiationDialog
-          isOpen={showNegotiationDialog}
-          onClose={() => {
-            setShowNegotiationDialog(false);
-            setSelectedProposal(null);
-          }}
-          proposal={selectedProposal}
-          onUpdate={fetchDashboardData}
-        />
-      )}
+{selectedProposal && showNegotiationDialog && (
+  <ProposalNegotiationDialog
+    isOpen={showNegotiationDialog}
+    onClose={() => {
+      setShowNegotiationDialog(false);
+      setSelectedProposal(null);
+    }}
+    proposal={selectedProposal}
+    onUpdate={fetchDashboardData}
+  />
+)}
 
-      {selectedProposal && showHistoryDialog && (
-        <ProposalHistoryDialog
-          isOpen={showHistoryDialog}
-          onClose={() => {
-            setShowHistoryDialog(false);
-            setSelectedProposal(null);
-          }}
-          proposalId={selectedProposal.id}
-        />
-      )}
+{selectedProposal && showHistoryDialog && (
+  <ProposalHistoryDialog
+    isOpen={showHistoryDialog}
+    onClose={() => {
+      setShowHistoryDialog(false);
+      setSelectedProposal(null);
+    }}
+    proposalId={selectedProposal.id}
+  />
+)}
 
-      {selectedProposal && showConfirmDialog && (
-        <ProposalConfirmDialog
-          isOpen={showConfirmDialog}
-          onClose={() => {
-            setShowConfirmDialog(false);
-            setSelectedProposal(null);
-          }}
-          action={confirmAction}
-          proposal={selectedProposal}
-          onConfirm={handleProposalStatusChange}
-        />
-      )}
-    </div>
-  );
-}
-```
-        
+{selectedProposal && showConfirmDialog && (
+  <ProposalConfirmDialog
+    isOpen={showConfirmDialog}
+    onClose={() => {
+      setShowConfirmDialog(false);
+      setSelectedProposal(null);
+    }}
+    action={confirmAction}
+    proposal={selectedProposal}
+    onConfirm={handleProposalStatusChange}
+  />
+)}
