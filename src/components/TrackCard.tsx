72156a6bae66ee8e097fe3b8c74392fdb1138dc0
iLi-Media-Group <theilimediagroup@gmail.com>
2025-06-05 10:25:50 -0300

    Updated TrackCard.tsx

--- src/components/TrackCard.tsx
+++ src/components/TrackCard.tsx
@@ -151,6 +151,7 @@ export function TrackCard({ track, onSelect }: TrackCardProps) {
           )}
 
           {/* Play Button */}
+          <button
             onClick={togglePlay}
             className="absolute inset-0 flex items-center justify-center bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity z-10"
           >
@@ -211,11 +212,6 @@ export function TrackCard({ track, onSelect }: TrackCardProps) {
           </div>
 
           {/* Action Buttons */}
-          <div className="flex items-center justify-between pt-2">
-            <AddToPlaylistButton trackId={track.id} variant="icon" />
-            
-            <button
-              onClick={(e) => {
           <div className="flex items-center justify-between pt-2">
             <AddToPlaylistButton trackId={track.id} variant="icon" />
             
